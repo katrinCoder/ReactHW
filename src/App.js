f8a6de5217f3c8653a73c656f0f58c01a1ab063f
Katrin <katrin.krotova@mail.ru>
2022-02-20 20:12:54 +0300

    First Homework

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Message } from './components'
+
+const text = 'Hello, it\'s my first homework'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HomeWork1</h1>
+      <Message text={text} />
+    </div>
+  );
+}
+
+export default App;
