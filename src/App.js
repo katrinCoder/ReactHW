32803ee888e3524f04d5b66d28ca7c53f695ef77
Katrin <katrin.krotova@mail.ru>
2022-02-24 19:57:24 +0300

    HomeWork2 1,2,3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import { Message } from './components'
-
-const text = 'Hello, it\'s my first homework'
+import { useState } from 'react'
 
 function App() {
+
+  const [messagelist, setMessageList] = useState([])
+  const [input, setInput] = useState('')
+  
+  const addMessage = (author, text) => {
+    const newMessageList = [...messagelist]
+    const newMessage = {
+      author,
+      text
+    }
+    newMessageList.push(newMessage)
+    setMessageList(newMessageList)
+  }
+
+  const eventChangeMessage = (event) => {
+    setInput(event.target.value)
+  }
+
+  const eventButtonSubmit = (event) => {
+    event.preventDefault()
+    addMessage('user', input)
+    setInput('')
+  }
+
   return (
     <div className="App">
-      <h1>HomeWork1</h1>
-      <Message text={text} />
+      <form onSubmit={eventButtonSubmit}>
+        <input
+          onChange={eventChangeMessage}
+          value={input}
+          type='text'
+          placeholder='Введите сообщение:'>
+        </input>
+        <button>Отправить</button>
+      </form>
+      <ul>
+        {
+          messagelist.map((message) => (
+            <li>
+              {message.author}: {message.text}
+            </li>
+          ))
+        }  
+      </ul>
     </div>
   );
 }
